132417e7bc4d08bf1cc42379334df34bd60b9dfa
Derrick R <dmr3p@mtmail.mtsu.edu>
2020-10-21 23:34:09 -0500

    Adding missing files from previous push

---
+++ Forklift Game/Assets/Scripts/Trigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Color m_oldColor = Color.green;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "yellow box 2")
+        {
+            Renderer render = GetComponent<Renderer>();
+            render.material.color = Color.yellow;
+        }
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = m_oldColor;
+    }
+}
