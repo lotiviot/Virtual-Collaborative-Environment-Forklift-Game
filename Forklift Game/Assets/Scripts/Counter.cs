23c8cec141415c0522b1657cae30790780d88504
lotiviot <54922399+lotiviot@users.noreply.github.com>
2020-11-10 21:45:15 -0500

    Menu Update

---
+++ Forklift Game/Assets/Scripts/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public string blue;
+    public string red;
+    public string yellow;
+    GameObject blueObj = GameObject.Find("Goal Suite/Goal Plane Blue");
+    int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blueObj != null)
+        {
+            counter++;
+        }
+    }
+}
